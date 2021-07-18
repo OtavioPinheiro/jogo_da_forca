b149585658bce4e1559cfc4e6dc559fc36030b05
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-07-17 23:35:09 -0300

    re-organizando

--- main.c
+++ main.c
@@ -1,50 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
+
 void abertura() {
     printf("*****************\n");
     printf("* Jogo de Forca *");
     printf("*****************\n");
 }
 
-void chuta(char chutes[26], int tentativas) {
+void chuta() {
     char chute;
+    printf("Qual letra? ");
     scanf(" %c", &chute);
 
     chutes[tentativas] = chute;
 }
 
-int main() {
-    char palavrasecreta[20];
+int jachutou(char letra) {
+    int achou = 0;
+    for (int i = 0; i < tentativas; ++i) {
+        if(chutes[i] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca() {
+    printf("Você já deu %d chutes\n", tentativas);
+
+    for (int i = 0; i < strlen(palavrasecreta); ++i) {
+        if(jachutou(palavrasecreta[i])) {
+            printf("%c ", palavrasecreta);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
+
+void escolhepalavra() {
     sprintf(palavrasecreta, "MELANCIA");
+}
 
+int main() {
     int acertou = 0;
-    int errou = 0;
+    int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
     abertura();
+    escolhepalavra();
 
     do {
-        for (int i = 0; i < strlen(palavrasecreta); ++i) {
-            int achou = 0;
-
-            for (int j = 0; j < tentativas; ++j) {
-                if (chutes[j] == palavrasecreta[i]) {
-                    achou = 1;
-                    break;
-                }
-            }
-
-            if (achou) {
-                printf("%c", palavrasecreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }
-        printf("\n");
-        chuta(chutes, tentativas);
+        desenhaforca();
+        chuta();
         tentativas++;
-
-    } while (!acertou && !errou);
+    } while (!acertou && !enforcou);
 }
